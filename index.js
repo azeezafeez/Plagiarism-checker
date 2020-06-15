347e6cdf0073be2bef9f1ba4d9c860ad8cc570b4
azeezafeez <chillingloccin1@gmail.com>
2020-06-15 10:39:08 +0100

    checking for plagiarism:

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const puppeteer= require('puppeteer');
 
-async function getBunnies(){
+async function getPlagiarism(){
   const browser = await puppeteer.launch({
     headless: false,   
     args: [ '--start-maximized', '--window-size=1920,1080' ],
@@ -20,9 +20,6 @@ async function getBunnies(){
 
   await page.waitForSelector("#textBox", { visible: true });
 
-
-  const input =  await page.$$("#textBox");
-  
   // Paste our article inside the textarea
   const article = 'BDD stands for Behaviour Driven Development. And TDD stands for Test Driven Development. These are actually not alternatives, even if they are thrown a lot into conversations these days.'+
     'TDD is a way to write code, irrespective of the architecture we implement. BDD is a variation of TDD with Behavioral approach to it. So these are not going to help us in implementing an architecture style.' +
@@ -31,40 +28,21 @@ async function getBunnies(){
   await page.$eval('#textBox', el => el.value = "BDD stands for Behaviour Driv");
 
   // Click the Check plagiarism button  
-  console.log("O' Favour!!  11");
-
-  await page.$eval(".g-recaptcha", el => {
+await page.$eval(".g-recaptcha", el => {
       console.log(el);
       el.click();
   });
 
-  console.log("O' Favour!!");
 
-
-  // console.log('Submit form');
-
-  // await page.waitForNavigation({waitUntil: 'networkidle2'});  
+ // await page.waitForNavigation({waitUntil: 'networkidle2'});  
 
   // console.log('Wait for nav form');
   
   // await page.$eval('#no_of_rzlt_fond', el => console.log(el.text()));
 
   
-  // Submit the form
-  // Get the results from checker
-  // Display the results (console.log)
-
   
   
-
-  // https://www.duplichecker.com/
-  // Use headless browser to visit website
-  
-  // Visit plagiarism checker webiste
-
-  //Paste content from body into plagiarism checker 
-
-  // Submit form on website and get results
 
   // Render formatted results back as api Response
 
@@ -80,4 +58,4 @@ async function getBunnies(){
 
 }
 
-getBunnies();
+getPlagiarism();
